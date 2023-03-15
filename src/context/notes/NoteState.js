31a72c89f16ba17f70bbd249fcfb2d82a2f1a590
Soumajyoti <soumyabwn3@gmail.com>
2023-03-15 23:29:20 +0530

    Adding and Deleting Function Done

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -13,7 +13,7 @@ const NoteState = (props) => {
             "__v": 0
         },
         {
-            "_id": "640f5ece7a949be05dddb49d",
+            "_id": "640f5ece7a949be05dddb49d5",
             "user": "640f5ea17a949be05dddb492",
             "title": "Inspired",
             "description": "Concentrte",
@@ -22,7 +22,7 @@ const NoteState = (props) => {
             "__v": 0
         },
         {
-            "_id": "640f5ece7a949be05dddb49d",
+            "_id": "640f5ece7a949be05dddb49d6",
             "user": "640f5ea17a949be05dddb492",
             "title": "Inspired",
             "description": "Concentrte",
@@ -31,7 +31,7 @@ const NoteState = (props) => {
             "__v": 0
         },
         {
-            "_id": "640f5ece7a949be05dddb49d",
+            "_id": "640f5ece7a949be05dddb49d7",
             "user": "640f5ea17a949be05dddb492",
             "title": "Inspired",
             "description": "Concentrte",
@@ -40,7 +40,7 @@ const NoteState = (props) => {
             "__v": 0
         },
         {
-            "_id": "640f5ece7a949be05dddb49d",
+            "_id": "640f5ece7a949be05dddb49d8",
             "user": "640f5ea17a949be05dddb492",
             "title": "Inspired",
             "description": "Concentrte",
@@ -49,7 +49,7 @@ const NoteState = (props) => {
             "__v": 0
         },
         {
-            "_id": "640f5ece7a949be05dddb49d",
+            "_id": "640f5ece7a949be05dddb49d9",
             "user": "640f5ea17a949be05dddb492",
             "title": "Inspired",
             "description": "Concentrte",
@@ -58,7 +58,7 @@ const NoteState = (props) => {
             "__v": 0
         },
         {
-            "_id": "640f5ece7a949be05dddb49d",
+            "_id": "640f5ece7a949be05dddb49d00",
             "user": "640f5ea17a949be05dddb492",
             "title": "Inspired",
             "description": "Concentrte",
@@ -67,7 +67,7 @@ const NoteState = (props) => {
             "__v": 0
         },
         {
-            "_id": "640f5ece7a949be05dddb49d",
+            "_id": "640f5ece7a949be05dddb49d12",
             "user": "640f5ea17a949be05dddb492",
             "title": "Inspired",
             "description": "Concentrte",
@@ -79,8 +79,37 @@ const NoteState = (props) => {
 
     const [notes, setNotes] = useState(notesInitial)
 
+    // Add a Note
+    const addNote = (title, description, tag) => {
+        // TODO Api Call
+        console.log("Adding a new Note")
+        const note = {
+            "_id": "640f5ece7a949be05dddb4ejgnj9d12",
+            "user": "640f5ea17a949be05dddb492",
+            "title": title,
+            "description": description,
+            "tag": tag,
+            "date": "2023-03-13T17:35:10.449Z",
+            "__v": 0
+        }
+        setNotes(notes.concat(note))
+    }
+
+    // Delete a Note
+    const deleteNote = (id) => {
+        const newNotes = notes.filter((note) => {
+            return note._id !== id
+        })
+        setNotes(newNotes)
+    }
+
+    // Edit a Note
+    const editNote = (id, title, description, tag) => {
+
+    }
+
     return (
-        <NoteContext.Provider value={{ notes, setNotes }}>
+        <NoteContext.Provider value={{ notes, addNote, editNote, deleteNote }}>
             {props.children}
         </NoteContext.Provider>
     )
